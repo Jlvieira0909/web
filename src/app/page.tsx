f3280444202c0bf6c9fccc5b0045cd83ea1eea93
Jlvieira0909 <lvjohn09@gmail.com>
2025-02-18 13:11:29 -0300

    "added more stuff"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { ArrowRight, Copy } from "lucide-react";
-import { Button } from "@/components/button";
+import { ArrowRight, Copy, Mail } from "lucide-react";
+import { Button } from "@/components/Button";
 import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
 
 export default function Home() {
   return (
@@ -12,6 +13,16 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
